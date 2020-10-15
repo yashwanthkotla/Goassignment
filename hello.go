c4c9b2747499e5d6a964f00af5fdabefa06dd360
yashwanthkotla <59574673+yashwanthkotla@users.noreply.github.com>
2020-10-15 23:38:32 +0530

    modification: Added result check function and modified other functions

--- hello.go
+++ hello.go
@@ -10,7 +10,6 @@ A structure that stores status of the new routine
 */
 type Future struct {
 	status          string
-	cancel          chan bool
 	futureCallReady chan bool
 }
 
@@ -18,84 +17,78 @@ type Future struct {
 Main function to start testing the future functions
 */
 func main() {
-	fmt.Println("Hello Universe")
-	futurepointer := setup_routine(print_something)
+	fmt.Printf("Hello Universe")
+	result := make(chan string)
+	futurepointer := setup_routine(print_something, result)
+	go add_done_callback(print_something_new, futurepointer)
 	// fmt.Println("\ncan it be cancelled:", routine_cancel(futurepointer))
 	time.Sleep(3 * time.Second)
-	fmt.Println("\nis add_done_callback done:", add_done_callback(print_something_new, futurepointer))
-	fmt.Println("\nis the 1st routine running:", is_routine_running(futurepointer))
-	fmt.Println("\nis 1st routine cancelled:", is_routine_cancelled(futurepointer))
-	time.Sleep(3 * time.Second)
-	fmt.Println("\nis the 1st routine done:", is_routine_done(futurepointer))
-	time.Sleep(5 * time.Second)
-	fmt.Println("\nis add_done_callback done:", add_done_callback(print_something_new, futurepointer))
+	fmt.Println("is the 1st routine running:  ", is_routine_running(futurepointer))
+	fmt.Println("is 1st routine cancelled:  ", is_routine_done(futurepointer))
+	fmt.Println("final result:  ", read_result(15, result))
+	fmt.Println("is 1st routine cancelled:  ", is_routine_cancelled(futurepointer))
+	fmt.Println("is the 1st routine done:  ", is_routine_done(futurepointer))
+	time.Sleep(15 * time.Second)
+	// go add_done_callback(print_something_new, futurepointer)
 }
 
 /*
 Method to setup and start routines, channels and pointers
 */
-func setup_routine(printsomething func()) *Future {
-	started := make(chan bool)
-	ended := make(chan bool)
-	cancel := make(chan bool)
+func setup_routine(printsomething func() string, result chan string) *Future {
 	futureCallReady := make(chan bool)
-	futureStatus := Future{"notstarted", cancel, futureCallReady}
+	futureStatus := Future{"notstarted", futureCallReady}
 	futurepointer := &futureStatus
-	go start_go_routine(printsomething, started, ended, cancel, futureCallReady)
-	go update_start_status(started, futurepointer)
-	go update_end_status(ended, futurepointer)
+	go start_go_routine(printsomething, result, futurepointer, futureCallReady)
+	go check_readiness_future(futureCallReady)
+	go check_future_Result(result)
 	return futurepointer
 }
 
 /*
-Method to read the channel data and update routine status
+Method to check readiness of adding new func
 */
-func update_start_status(started chan bool, pointer *Future) {
-	startedorcancelled := <-started
-	if startedorcancelled == true {
-		pointer.status = "started"
-	}
+func check_future_Result(result chan string) {
+	resp := <-result
+	result <- resp
 }
 
 /*
-Method to read the channel data and update routine status
+Method to check readiness of adding new func
 */
-func update_end_status(ended chan bool, pointer *Future) {
-	endedorcancelled := <-ended
-	if endedorcancelled == true {
-		pointer.status = "ended"
-	}
+func check_readiness_future(futureCallReady chan bool) {
+	<-futureCallReady
+	futureCallReady <- true
 }
 
 /*
 Method to run the actual function
 */
-func start_go_routine(actual_function func(), started chan bool,
-	ended chan bool, cancel chan bool, futureCallReady chan bool) {
-	select {
-	case iscancelledValue := <-cancel:
-		close(started)
-		fmt.Printf("routine cancelled\n", iscancelledValue)
-		close(ended)
+func start_go_routine(actual_function func() string, result chan string,
+	future *Future, futureCallReady chan bool) {
+	if future.status == "cancelled" {
+		fmt.Printf("\nroutine cancelled")
+		close(result)
 		futureCallReady <- true
-	default:
-		started <- true
-		fmt.Printf("routine started\n")
-		close(cancel)
-		actual_function()
-		ended <- true
+	} else {
+		future.status = "started"
+		fmt.Printf("\nroutine started")
+		resp := actual_function()
+		future.status = "ended"
 		futureCallReady <- true
+		result <- resp
 	}
 }
 
 /*
 A dummy function
 */
-func print_something() {
+func print_something() string {
 	for a := 0; a < 10; a++ {
 		fmt.Printf("\nvalue of a: %d", a)
 		time.Sleep(time.Second)
 	}
+	return "I am done"
 }
 
 /*
@@ -103,8 +96,7 @@ Method to cancel the routine
 */
 func routine_cancel(pointer *Future) bool {
 	if pointer.status == "notstarted" {
-		fmt.Printf("\n trying to cancel func\n")
-		pointer.cancel <- true
+		fmt.Printf("\n trying to cancel func")
 		pointer.status = "cancelled"
 		return true
 	} else {
@@ -151,12 +143,12 @@ Method to add new function after the completion of the first function
 */
 func add_done_callback(newfunc func(), pointer *Future) bool {
 	if pointer.status == "ended" || pointer.status == "cancelled" {
-		fmt.Printf("Started new func")
+		fmt.Printf("\nStarted new func")
 		newfunc()
 	} else {
-		fmt.Printf("Started new func")
+		fmt.Printf("\nStarted new func")
 		<-pointer.futureCallReady
-		go newfunc()
+		newfunc()
 	}
 	return true
 }
@@ -166,16 +158,20 @@ Another dummy method
 */
 func print_something_new() {
 	for a := 0; a < 5; a++ {
-		fmt.Printf("new value of a: %d\n", a)
+		fmt.Printf("\nnew value of b: %d", a)
 		time.Sleep(time.Second)
 	}
 }
 
 /*
-Method to read result from a process
-
-func read_result(timeout int){
-	time.Sleep(time.Duration(timeout)*time.Second)
-
-}
+Method to read result
 */
+func read_result(timeout int, result chan string) string {
+	time.Sleep(time.Duration(timeout) * time.Second)
+	select {
+	case resp := <-result:
+		return resp
+	default:
+		panic("no result")
+	}
+}
